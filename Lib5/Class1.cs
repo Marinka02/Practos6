a13250013db8beee78da38a9eb63850c874f1dc2
исп-31 <-31@LAB17-05>
2020-11-17 09:40:06 +0300

    Добавьте файлы проекта.

---
+++ Lib5/Class1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lib_5
+{
+    public class Triangle
+    {
+        int a; // сторона а
+        int b; // сторона б
+        int c; // сторона с
+
+        // свойство а
+        public int A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                // проверяем что значение больше 0
+                if (value > 0) 
+                {
+                    a = value;
+                }
+                else
+                {
+                    MessageBox.Show("Значение должно быть больше нуля");
+                }
+            }
+        }
+        // свойство б
+        public int B
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                // проверяем что значение больше 0
+                if (value > 0)
+                {
+                    b = value;
+                }
+                else
+                {
+                    MessageBox.Show("Значение должно быть больше нуля");
+                }
+            }
+        }
+        // свойство с
+        public int C
+        {
+            get
+            {
+                return c;
+            }
+            set
+            {
+                // проверяем что значение больше 0
+                if (value > 0)
+                {
+                    c = value;
+                }
+                else
+                {
+                    MessageBox.Show("Значение должно быть больше нуля");
+                }
+            }
+        }
+        // конструктор с параметрами
+        public Triangle(int valueA, int valueB, int valueC)
+        {
+            if (valueA > 0 && valueB > 0 && valueC > 0)
+            {
+                a = valueA;
+                b = valueB;
+                c = valueC;
+            }
+            else
+            {
+                MessageBox.Show("Значения должны быть больше нуля");
+            }
+        }
+        // конструктор по умолчанию
+        public Triangle()
+        {
+            a = 4;
+            b = 5;
+            c = 7;
+        }
+        // метод расчета периметра
+        public int GetPerimetr()
+        {
+            int p = a + b + c;
+            return p;
+        }
+        // метод задания значений
+        public void SetParams(int valueA, int valueB, int valueC)
+        {
+            if (valueA > 0 && valueB > 0 && valueC > 0)
+            {
+                a = valueA;
+                b = valueB;
+                c = valueC;
+            }
+            else
+            {
+                MessageBox.Show("Значения должны быть больше нуля");
+            }
+        }
+        // метод увеличения сторон в 2 раза
+        public void SetParams()
+        {
+            // проверка что значение не превысят максимальный размер типа int
+            if (a * 2 == -2147483648  || b * 2 == -2147483648 || c * 2 == -2147483648)
+            {
+                MessageBox.Show("При увеличении значения превысят максимальный размер типа int!");
+            }
+            else
+            {
+                a *= 2;
+                b *= 2;
+                c *= 2;
+            }
+        }
+        public static bool operator true(Triangle triangle)
+        {
+            // проверяем существует ли треугольник
+            if (triangle.A + triangle.B > triangle.C && triangle.B + triangle.C > triangle.A && triangle.A + triangle.C > triangle.B)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator false(Triangle triangle)
+        {
+            return false; //???
+        }
+        public static Triangle operator ++(Triangle triangle)
+        {
+            Triangle copy = new Triangle();
+            copy = triangle;
+            if (copy.A + 1 == -2147483648 || copy.B + 1 == -2147483648 || copy.C + 1 == -2147483648)
+            {
+                MessageBox.Show("При увеличении значения превысят максимальный размер типа int!");
+                return triangle; 
+            }
+            else
+            {
+                copy.A++;
+                copy.B++;
+                copy.C++;
+            }
+            return copy;
+        }
+        public static Triangle operator --(Triangle triangle)
+        {
+            Triangle copy = new Triangle();
+            copy = triangle;
+            if (copy.A - 1 > 0 && copy.B - 1 > 0 && copy.C - 1 > 0) 
+            {
+                copy.A--;
+                copy.B--;
+                copy.C--;
+
+            }
+            else
+            {                
+                MessageBox.Show("При изменении хотя бы одно значение станет равно 0");
+            }
+            return copy;
+        }
+    }
+}
---
+++ Var5/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lib_5;
+
+namespace Var5
+{
+    public partial class Form1 : Form
+    {
+        // описали глобальный объект класса Triangle
+        Lib_5.Triangle triangle;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // о программе
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Выполнила Ефимкина Марина. ИСП-31. Вариант 5:Создать класс Triangle (треугольник) с полями-сторонами. Создать необходимые методы и свойства. Определить метод вычисления периметра." +
+                " Создать перегруженные методы SetParams, для установки параметров объекта, в том числе увеличения размеров треугольника в 2 раза.");
+        }
+        // выход
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           this.Close();
+        }
+        // периметр
+        private void периметрToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(triangle)
+            {            
+                int p;
+                // вызываем функцию из класса
+                p = triangle.GetPerimetr();
+                // выводим на форму
+                textBoxPerimetr.Text = Convert.ToString(p);
+
+            }
+            else
+            {
+                MessageBox.Show("Треугольник не существует");
+            }
+        }
+        // увеличение сторон в 2 раза
+        private void увеличениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // вызываем метод увеличения в 2 раза
+            triangle.SetParams();
+            // обновляем текущие значения
+            textBoxStoronaA.Text = triangle.A.ToString();
+            textBoxstoronaB.Text = triangle.B.ToString();
+            textBoxStaranaC.Text = triangle.C.ToString();
+        }
+        // сброс
+        private void сброситьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // пересоздаем объект со стандартными значениями
+            triangle = new Triangle();
+            // обновляем текущие значения
+            textBoxStoronaA.Text = triangle.A.ToString();
+            textBoxstoronaB.Text = triangle.B.ToString();
+            textBoxStaranaC.Text = triangle.C.ToString();
+            // очищаем поля ввода 
+            textBoxA.Clear();
+            textBoxB.Clear();
+            textBoxC.Clear();
+        }
+        // при загрузке формы создаем объект со стандартными значениями
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // создаем объект со стандартными значениями
+            triangle = new Triangle();
+            // выводим текущие значения
+            textBoxStoronaA.Text = triangle.A.ToString();
+            textBoxstoronaB.Text = triangle.B.ToString();
+            textBoxStaranaC.Text = triangle.C.ToString();
+        }
+        // изменить значения
+        private void изменитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(textBoxA.Text, out int a) && Int32.TryParse(textBoxB.Text, out int b) && Int32.TryParse(textBoxC.Text, out int c))
+            {
+                // обращаемся к методу
+                triangle.SetParams(a, b, c);
+                // выводим текущие значения
+                textBoxStoronaA.Text = triangle.A.ToString();
+                textBoxstoronaB.Text = triangle.B.ToString();
+                textBoxStaranaC.Text = triangle.C.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Ошибка конвертации");
+            }
+        }
+
+        private void увеличениеНа1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            triangle++;
+            // выводим текущие значения
+            textBoxStoronaA.Text = triangle.A.ToString();
+            textBoxstoronaB.Text = triangle.B.ToString();
+            textBoxStaranaC.Text = triangle.C.ToString();
+        }
+
+        private void уменьшениеНа1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            triangle--;
+            // выводим текущие значения
+            textBoxStoronaA.Text = triangle.A.ToString();
+            textBoxstoronaB.Text = triangle.B.ToString();
+            textBoxStaranaC.Text = triangle.C.ToString();
+        }
+
+        private void существуетЛиТреугольникToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(triangle)
+            {
+                MessageBox.Show("Треугольник существует");
+            }
+            else
+            {
+                MessageBox.Show("Треугольник не существует");
+            }
+        }
+    }
+}
